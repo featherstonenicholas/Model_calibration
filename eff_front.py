1b1c67ac013f8fae70171f719cc334f224587e7e
Nicholas Featherstone <featherstonenicholas@gmail.com>
2024-05-22 15:09:44 +0100

    update

--- eff_front.py
+++ eff_front.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 import datetime as dt
+import pandas as pd
 from pandas_datareader import data as pdr
 import yfinance as yf
 import scipy.optimize as sc
@@ -17,7 +18,6 @@ def getData(stocks,start,end):
     meanReturns=returns.mean()
     covMatrix=returns.cov()
     return meanReturns,covMatrix
-
 
 # define function to calculate performance of portfolio
 
@@ -39,26 +39,44 @@ def maxSR(meanReturns, covMatrix,riskFreeRate=0,constraintSet=(0,1)):
     bounds=tuple(bound for asset in range (numAssets))
     result = sc.minimize(negSR, numAssets*[1./numAssets], args=args, method='SLSQP', bounds=bounds, constraints=constraints)
     return result
-#define stock list and get summary statistics
 
+def portfolioVariance(weights, meanReturns,covMatrix):
+    return portfolioPerformance(weights, meanReturns,covMatrix)[1]
+
+def minVar(meanReturns, covMatrix,riskFreeRate=0,constraintSet=(0,1)):
+    #minimize variance by altering weights
+    numAssets=len(meanReturns)
+    args=(meanReturns,covMatrix)
+    constraints= ( {'type':'eq' , 'fun' : lambda x: np.sum(x) -1})
+    bound=constraintSet
+    bounds=tuple(bound for asset in range (numAssets))
+    result = sc.minimize(portfolioVariance, numAssets*[1./numAssets], args=args, method='SLSQP', bounds=bounds, constraints=constraints)
+    return result
+    
+#define stock list and get summary statistics
 stocklist=['CBA','BHP','TLS']
 stocks=[stock+'.AX' for stock in stocklist]
 endDate=dt.datetime.now()
 startDate= endDate-dt.timedelta(days=365)
 meanReturns , covMatrix = getData(stocks,startDate,endDate)
 
-#define a weighting vector
-weights = np.array([0.3, 0.4 , 0.3])
-
-#calculate performance of this portfolio
-
-returns, std = portfolioPerformance(weights,meanReturns, covMatrix)
-#print ( round(100*returns,2), round(100*std,2))
-
-#calculate max SR portfolio
-result= maxSR(meanReturns, covMatrix)
-print(result)
 
 
-
+def calculatedResults(meanReturns, covMatrix,riskFreeRate=0,constraintSet=(0,1)):
+    #Output Max SR, Min Volatility, efficient frontier
+    maxSR_Portfolio= maxSR(meanReturns, covMatrix)
+    maxSR_Returns, maxSR_std = portfolioPerformance(maxSR_Portfolio['x'], meanReturns,covMatrix)
+    maxSR_Returns, maxSR_std= round(100*maxSR_Returns,2), round(100*maxSR_std,2)
+    maxSR_allocation=pd.DataFrame(maxSR_Portfolio['x'],index=meanReturns.index,columns=['allocation'])
+    maxSR_allocation.allocation=[round(i*100,1) for i in maxSR_allocation.allocation]
     
+    minVar_Portfolio= minVar(meanReturns, covMatrix)
+    minVar_Returns, minVar_std = portfolioPerformance(minVar_Portfolio['x'], meanReturns,covMatrix)
+    minVar_Returns, minVar_std= round(100*minVar_Returns,2), round(100*minVar_std,2)
+    minVar_allocation=pd.DataFrame(minVar_Portfolio['x'],index=meanReturns.index,columns=['allocation'])
+    minVar_allocation.allocation=[round(i*100,1) for i in minVar_allocation.allocation]
+    return maxSR_Returns, maxSR_std, maxSR_allocation ,minVar_Returns, minVar_std, minVar_allocation
+
+result= calculatedResults(meanReturns, covMatrix,riskFreeRate=0,constraintSet=(0,1))
+a,b= result[2], result[5]
+print(a+b)
