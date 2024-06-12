e39248b4e0da6602f47eca06d6905f046e7f7de6
Nicholas Featherstone <featherstonenicholas@gmail.com>
2024-06-12 07:38:54 +0100

    Plot volatility surface, work in oprogress

--- vol_surface.py
+++ vol_surface.py
@@ -1,51 +1,75 @@
-import os
-import time
-import pickle
-import numpy as np
+import yfinance as yf
+import warnings
 import pandas as pd
-import concurrent.futures as cf
+import numpy as np
+import datetime as dt
 import matplotlib.pyplot as plt
-from datetime import timedelta, datetime, date
-from thetadata import ThetaClient, OptionReqType, OptionRight, DateRange, DataType
+from matplotlib import cm
 
-your_username = 'featherstonenicholas@gmail.com'
-your_password = 'G@ndalf41223'
-def get_expirations(root_ticker) -> pd.DataFrame:
-    """Request expirations from a particular options root"""
-    # Create a ThetaClient
-    client = ThetaClient(username=your_username, passwd=your_password, timeout=15)
-    # Connect to the Terminal
-    with client.connect():
-        # Make the request
-        data = client.get_expirations(
-            root=root_ticker,
-        )
-    return data
-root_ticker = 'SPY'
-expirations = get_expirations(root_ticker)
+# for interactive figures
+#%matplotlib notebook
+from mpl_toolkits.mplot3d import Axes3D
+from scipy import interpolate
+
+warnings.filterwarnings("ignore")
+
+# Get the options data for a specific stock
+asset = yf.Ticker("AAPL")
+expirations=asset.options
+#get current price of stock and round to nearest 5 to prep for our Strike mesh
+price = asset.info['currentPrice']
+price=int(round(price/5.0)*5)
+
+#create a list of strikes around current price
+all_strikes = np.arange(price-50, price +55, 5)
+
+#create a list of expirations in years
+expirations_years=[]
 
 
-time_now=datetime.today()
-exp_dates = expirations[expirations > time_now + timedelta(days=7)]
-print(exp_dates)
 
-def get_strikes(root_ticker, expiration_date) -> pd.DataFrame:
-    """Request strikes from a particular option contract"""
-    # Create a ThetaClient
-    client = ThetaClient(username=your_username, passwd=your_password, timeout=15)
-    # Connect to the Terminal
-    with client.connect():
-        # Make the request
-        data = client.get_strikes(
-            root=root_ticker,
-            exp=expiration_date
-        )
-    return data
-root_ticker = 'SPY'
-exp_date = date(2022,9,16)
-strikes = get_strikes(root_ticker, exp_date)
-print(strikes)
+Z_p = np.empty([len(expirations), len(all_strikes)])
 
-all_strikes = {}
-for exp_date in exp_dates:
-    all_strikes[exp_date] = pd.to_numeric(get_strikes(root_ticker, exp_date))
+error_expirations=[]
+for i in range(len(expirations)):
+    expiration=expirations[i]
+    opt=asset.option_chain(expiration)
+    calls= opt.calls
+ 
+    calls['mid'] = calls[['bid','ask']].mean(axis=1)
+    price=calls['mid']
+    s=calls['strike']
+    sMin=min(s)
+       
+    #get the maturity in years
+    T=(pd.to_datetime(expiration)-dt.datetime.today()).days/365
+    expirations_years.append(T)
+    f = interpolate.interp1d(s, price, bounds_error=False, fill_value=0)
+    Z_p[i, :] = f(all_strikes) 
+   
+    #check for expirations which will create holes in our mesh
+    if sMin > min(all_strikes):
+       error_expirations.append(i) 
+print('Number of error maturities: '+str(len(error_expirations)))
+print(len(expirations_years))
+print(len(Z_p))
+for i in error_expirations:
+    print(i)
+    Z_p=np.delete(Z_p,i,0)
+    expirations_years=np.delete(expirations_years,i)
+print(len(expirations_years))
+print(len(Z_p))   
+    
+# define a grid for the surface
+X , Y = np.meshgrid(all_strikes, expirations_years)  
+
+# plot the surface
+fig = plt.figure(figsize=(8,6))
+ax = fig.add_subplot(111, projection='3d')
+ax.plot_surface(X, Y, Z_p, cmap=cm.coolwarm)
+ax.set_ylabel('Maturity (years)') 
+ax.set_xlabel('Strike') 
+ax.set_zlabel('C(K, T)')
+ax.set_title('Apple Call prices')
+plt.savefig('fig3.png')
+plt.show()
